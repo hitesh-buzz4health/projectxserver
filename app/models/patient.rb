36a2b0df20bdc61ffda201d1a3ef3707346c60c6
sonal <sonal@buzz4health.com>
2017-07-06 12:51:34 +0530

    inclusion of user ids in searchable block.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -26,6 +26,7 @@ class Patient
   searchable do 
     text :name ,      :stored => true
     text :email_id ,  :stored => true
+    text :user_ids  , :stored => true
   end 
 
   after_save { |obj| Sunspot.index!(obj) }
